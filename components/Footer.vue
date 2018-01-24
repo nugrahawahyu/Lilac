052ff1ac2b532929fa33fa8d333ec8b27aa8b8bb
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2018-01-24 22:23:16 +0700

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <a href="https://twitter.com/wahyucnugraha" target="_blank">Twitter</a>
+    &middot;
+    <a href="https://github.com/nugrahawahyu" target="_blank">Github</a>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  color: #fff;
+
+  a {
+    color: #fff;
+    text-decoration: underline;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <div class="content">
+      <p>"We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves."</p>
+      <p>- Budha</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: 'Users'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin: 30px 0;
+}
+.users
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.user
+{
+  margin: 10px 0;
+}
+.content {
+  padding: 16px;
+}
+p {
+  color: #fff;
+}
+</style>
+
+<style>
+body {
+  background-color: #000;
+  min-height: 100vh;
+}
+</style>
