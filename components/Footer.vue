722073f831df81c7e56bda4a54e10c72f0f86172
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2018-04-25 14:08:31 +0700

    update homepage

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,6 +5,16 @@
         Website by
         <a href="https://twitter.com/wahyucnugraha" target="_blank">Wahyu Nugraha</a>
       </div>
+      <div class="mdl-cell mdl-cell--12-col" style="margin-top: 0;">
+        <ul class="social-collection social-collection">
+          <li>
+            <a href="https://github.com/nugrahawahyu"><i class="fab fa-github"></i></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/wahyucnugraha"><i class="fab fa-twitter"></i></a>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 </template>
@@ -21,6 +31,21 @@
     font-weight: 600;
     color: #666;
     text-decoration: none;
+  }
+}
+
+.social-collection {
+  margin: 0;
+  list-style: none;
+
+  &--inline {
+    display: inline-block;
+  }
+
+  li {
+    font-size: 1rem;
+    display: inline-block;
+    margin-right: 24px;
   }
 }
 </style>
---
+++ partials/homepage/v1.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="page-container">
+    <section class="dark-section">
+      <div class="content content--full-height" style="padding: 0;">
+        <parallax img-src="images/banner.jpg" style="position: relative;">
+          <div style="position: absolute; background-color: rgba(255,255,255,0.5); top: 0; bottom: 0; left: 0; right: 0; z-index: 1;">
+            <div class="header-content content content--narrow">
+              <h1 class="title"><span style="font-weight: 800;">Hi</span>, you found me</h1>
+            </div>
+          </div>
+        </parallax>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content content--narrow">
+        <h2>Tweets</h2>
+        <no-ssr>
+          <div>
+            <a href="https://twitter.com/wahyucnugraha?ref_src=twsrc%5Etfw"
+              class="twitter-follow-button"
+              data-show-count="false">
+              Follow @wahyucnugraha
+            </a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+        </no-ssr>
+        <no-ssr>
+          <div>
+            <a class="twitter-timeline"
+              href="https://twitter.com/wahyucnugraha?ref_src=twsrc%5Etfw"
+              data-tweet-limit="3">
+              Tweets by wahyucnugraha
+            </a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+        </no-ssr>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Parallax from '~/components/Parallax'
+
+export default {
+  components: {
+    Parallax
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 42px;
+  margin-top: 100px;
+}
+
+h2 {
+  font-size: 28px;
+}
+
+.header-content {
+  text-align: center;
+}
+
+.title
+{
+  color: #fff;
+  font-family: "Escrow Condensed", georgia, times, "\30E1\30A4\30EA\30AA", "Meiryo", "\30D2\30E9\30AE\30CE\89D2\30B4   Pro W3", "Hiragino Kaku Gothic Pro", "\FF2D\FF33\FF30\30B4\30B7\30C3\30AF", "MS PGothic", serif;
+}
+.users
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.user
+{
+  margin: 10px 0;
+}
+.content {
+  margin: 0 auto;
+  padding: 16px;
+
+  &--narrow {
+    max-width: 400px;
+  }
+
+  &--full-height {
+    height: 80vh;
+  }
+}
+
+.mystic-section {
+  background-color: #e6ecf0;
+}
+
+.dark-section {
+  background-color: #000;
+  color: #fff;
+}
+</style>
